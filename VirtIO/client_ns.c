e5ebdf6c33d75f913722e5140dc2cafee3b29aed
Zsirong <51980862+Zsirong@users.noreply.github.com>
2023-04-04 14:11:56 +0800

    update

--- VirtIO/client_ns.c
+++ VirtIO/client_ns.c
@@ -11,9 +11,9 @@
 #include <sys/timex.h>
 
 #define MAXFILE 65535
-#define CONTROL_PORT "/dev/vport1p3"
-#define OFFSET_LOG "/home/zsr/Inter-VM-PTP/VirtIO/offset_ns.log"
-#define Debug_LOG "/home/zsr/Inter-VM-PTP/VirtIO/debug_ns.log"
+#define CONTROL_PORT "/dev/vport1p4"
+#define OFFSET_LOG "/home/zsr/Desktop/Inter-VM-PTP/VirtIO/offset_ns.log"
+#define Debug_LOG "/home/zsr/Desktop/Inter-VM-PTP/VirtIO/debug_ns.log"
 
 void getTheTime( char* str, struct timespec *time);
 long getOffset(struct timespec t1, struct timespec t2, struct timespec t3, struct timespec t4);
@@ -25,114 +25,130 @@ void write_info(struct timespec time[][4],int n,int fd_log);
 
 int main()
 {
-//      struct timeval t1, t2, t3, t4;
-      struct timeval time[11][4];
-      struct timespec time_ns[11][4];
+      struct timeval time[2][4];
+      struct timespec time_ns[2][4];
       int fd_con;    //通信文件
       int fd_log;    //记录文件
       int fd_debug;   //调试文件
       char buf_log[128];
-      int ret , t;
+      int ret ,set_ret, t;
       long AveOffset,AveDelay;
-      long offsettime[11];
+      long offsettime[2];
+      int status = 0;        ////状态，0为波动状态，1为稳定状态
       int count = 0;
       int flag = 1;
+      int exp_count = 0;
+      
 
+            //打开记录测试过程的文件
+      if((fd_debug=open(Debug_LOG,O_CREAT|O_WRONLY|O_APPEND,0600))<0)
+      {
+            printf("open file err\n");
+            exit(0);
+      }
+      
+      fd_con = open(CONTROL_PORT, O_RDWR);
+      if(fd_con == -1){
+        fprintf(stderr,"%d:can't open vport\n",errno);
+        return -1;
+      }
+      
+      if((fd_log=open(OFFSET_LOG,O_CREAT|O_WRONLY|O_APPEND,0600))<0)
+      {
+            printf("open file err\n");
+            exit(0);
+      }
+      
       while(1)
       {
-            //打开记录测试过程的文件
-//          if((fd_debug=open(Debug_LOG,O_CREAT|O_WRONLY|O_APPEND,0600))<0)
-//          {
-//                printf("open file err\n");
-//                exit(0);
-//          }
       
         t=1;
-        //记录10次测试时间
-        fd_con = open(CONTROL_PORT, O_RDWR);
-        if(fd_con == -1){
-          fprintf(stderr,"%d:can't open vport\n",errno);
-          return -1;
-        }
       
-     	while(t>=0){
+     	 while(t>=0){
           //获取主钟发来的第一次消息，记录t1,t2
-	  ret = read(fd_con,&time_ns[t][0],sizeof(struct timespec));
-//          gettimeofday(&time[t][1],NULL);
-	  clock_gettime(CLOCK_REALTIME,&time_ns[t][1]);
+			   ret = read(fd_con,&time_ns[t][0],sizeof(struct timespec));
+			   clock_gettime(CLOCK_REALTIME,&time_ns[t][1]);
           
-          if(ret == -1){
+           if(ret == -1){
               fprintf(stderr,"can't write data from guestOS\n");
               return -1;
-          }
+           }
 
 //          sprintf(buf_log,"buffer(t1):%ld,%ld\n",time_ns[t][0].tv_sec,time_ns[t][0].tv_nsec);
 //          write(fd_debug,buf_log,strlen(buf_log));
 
           //发送消息给主钟，记录t3
-//	  gettimeofday(&time[t][2],NULL);
-	  clock_gettime(CLOCK_REALTIME,&time_ns[t][2]);
-	  write(fd_con,&time_ns[t][2],sizeof(struct timespec));
-          if(ret == -1){
+			    clock_gettime(CLOCK_REALTIME,&time_ns[t][2]);
+			    write(fd_con,&time_ns[t][2],sizeof(struct timespec));
+            if(ret == -1){
               fprintf(stderr,"can't write data from guestOS\n");
               return -1;
-          }
+            }
 
           //获取主钟消息，记录t4
-	  ret = read(fd_con,&time_ns[t][3],sizeof(struct timespec));
-          if(ret == -1){
+			    ret = read(fd_con,&time_ns[t][3],sizeof(struct timespec));
+            if(ret == -1){
               fprintf(stderr,"can't write data from guestOS\n");
               return -1;
-          }
+            }
           
 //          sprintf(buf_log,"buffer(t4):%ld,%ld\n",time_ns[t][3].tv_sec,time_ns[t][3].tv_nsec);
 //          write(fd_debug,buf_log,strlen(buf_log));
   
-          t = t - 1;
-      }
+            t--;
+        }
          //计算平均时间偏差
-      AveOffset = getAveOffset(time_ns,1);
-      AveDelay = getAveDelay(time_ns,1);
-      printf("offset: %ld ns, delay: %ld ns\n",AveOffset,AveDelay);
-      //printf("Before, the average offset is %ld us\n", beforeOffset);
+        AveOffset = getAveOffset(time_ns,1);
+        AveDelay = getAveDelay(time_ns,1);
           
-          //根据时间偏差修改系统时间
-      if ( flag == 1 || AveDelay <= 10000){
-      	printf("setting, flag = %d, count = %d, AveDelay = %ld\n", flag, count, AveDelay);
-      	flag = 0 ;
-      	count = 0 ;
-      	ret = setTheTime(AveOffset);
-      	if(ret == -1){
-          	fprintf(stderr,"%d:set the time fail\n", errno);
-          	return -1;
-      	}
-      }else{
-      	printf("not setting, flag = %d, count = %d, AveDelay = %ld\n", flag, count, AveDelay);
-      	count++;
-      	if( count == 12){
-      		flag = 1;
-      		count = 1;
-      	}
-      	
-      }
-//      printf("修改参数为:%d\n", ret);
-//      sprintf(buf_log,"修改参数为:%d\n\n", ret);
-//          write(fd_debug,buf_log,strlen(buf_log));
+        if( count == 10 || ( AveOffset >= -1000 && AveOffset <= 1000) ){
+      	  flag = 1;
+      	  count = 0;
+      	  if(AveOffset >= -1000 && AveOffset <= 1000){
+      	    //1为稳定状态
+      	    status = 1;
+      	  }else{
+      	  	status = 0;
+      	  }
+        }
+        //根据时间偏差修改系统时间
+        if( flag == 1 ){
+         	printf("offset: %ld ns, delay: %ld ns\n",AveOffset,AveDelay);
+      	  set_ret = setTheTime(AveOffset);
+      	  if(set_ret == -1)
+      	  {
+            fprintf(stderr,"%d:set the time fail\n", errno);
+            return -1;
+     	  	}    
+      	  //printf("修改参数为:%d\n", set_ret);
+      	  if(status == 1){
+      	  	//只记录稳定状态的offset
+      	    sprintf(buf_log,"the average offset is %ld ns \n",AveOffset);
+            write(fd_log,buf_log,strlen(buf_log));
+            exp_count++;
+      	  }
+      	  flag = 0;
+      	  count = 0;
+        }else{
+      	  count++;
+      	  printf("waitng...%d\n",count);
+      	  write_info(time_ns,1,fd_debug);
+        }
         
-        //将时间偏差记录在log文件里
-          if((fd_log=open(OFFSET_LOG,O_CREAT|O_WRONLY|O_APPEND,0600))<0)
-          {
-                printf("open file err\n");
-                exit(0);
-          }
-          write_info(time_ns,1,fd_log);
-          sprintf(buf_log,"the average offset is %ld ns \n",AveOffset);
-          write(fd_log,buf_log,strlen(buf_log));
-          close(fd_con);
-          close(fd_debug);
-          close(fd_log);
-          sleep(5);
+        
+//        write_info(time_ns,1,fd_log);
+        sprintf(buf_log,"the average offset is %ld ns \n\n",AveOffset);
+        write(fd_debug,buf_log,strlen(buf_log));
+          
+        if(exp_count == 1000){
+        	return -1;
+        }
+
       }
+      
+      close(fd_con);
+      close(fd_debug);
+      close(fd_log);
 }
 
 
@@ -220,19 +236,26 @@ int setTheTime(long offset){
   long result;
   int ret;
   
+//  offset = offset / 2 ;
+  
   clock_gettime(CLOCK_REALTIME,&time_ns);
+  
+//  printf("1.settime sec:%ld,usec:%ld\n",time_ns.tv_sec,time_ns.tv_nsec);
   
   time_ns.tv_sec -= (offset) / 1000000000;
   time_ns.tv_nsec -= (offset) % 1000000000;
+  
+//  printf("2.settime sec:%ld,usec:%ld\n",time_ns.tv_sec,time_ns.tv_nsec);
   
   if(time_ns.tv_nsec >= 1000000000){
   	time_ns.tv_sec += time_ns.tv_nsec / 1000000000;
   	time_ns.tv_nsec = time_ns.tv_nsec % 1000000000 ;
   }else if(time_ns.tv_nsec <0){
-  	time_ns.tv_sec -= time_ns.tv_nsec / 1000000000;
-  	time_ns.tv_nsec = time_ns.tv_nsec % 1000000000 ;
+  	time_ns.tv_sec -= time_ns.tv_nsec / 1000000000 + 1;
+  	time_ns.tv_nsec = 1000000000 + time_ns.tv_nsec ;
   }
 
+//  printf("3.settime sec:%ld,usec:%ld\n",time_ns.tv_sec,time_ns.tv_nsec);
   ret = clock_settime(CLOCK_REALTIME,&time_ns);
   return ret;
 }
